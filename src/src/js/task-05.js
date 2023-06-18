5f4ecf83dde9f217d082941b6b00d73e98955a6c
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-06-18 22:58:57 +0300

    Task5

---
+++ src/src/js/task-05.js
@@ -0,0 +1,13 @@
+const inputEl = document.querySelector('#name-input')
+
+const spanEl = document.querySelector("#name-output")
+
+inputEl.addEventListener('input', onInput)
+
+function onInput() {
+    
+    spanEl.textContent = inputEl.value;
+    if (!inputEl.value) {
+        spanEl.textContent="Anonymous"
+    }
+}
