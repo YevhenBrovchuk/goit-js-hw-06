c3d37170cfd850a9abb594a1ce7c4b0a4bb5e4d8
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-06-19 00:28:47 +0300

    Task8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,18 @@
 
+const formEl = document.querySelector(".login-form")
+let info={}
+formEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(ev) {
+    ev.preventDefault();
+    const {
+        elements: { email, password }
+    } = ev.currentTarget;
+if (email.value === "" || password.value === "") {
+    return alert ("Please fill in all the fields!");
+  }
+    info.email = email.value;
+    info.password = password.value;
+   formEl.reset();
+    
+}
