b1aed7c08e61a7ff58d54758dd9530b5c8bfff34
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-06-19 02:03:59 +0300

    недороблена Task10

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const inputEl = document.querySelector('input ')
+const btnCreateEl = document.querySelector("[data-create]")
+const btnDestroyEl = document.querySelector('[data-destroy]')
+const divEl=document.querySelector("#boxes")
+
+btnCreateEl.addEventListener("click", onClickCreate)
+// btnDestroyEl.querySelector("click", onClickDestroy)
+
+
+function onClickCreate() {
+  createBoxes(Number(inputEl.value))
+  
+}
+
+
+
+
+
+function createBoxes(amount) {
+  const arrdiv = []
+  let qwe=0
+  for (let i = 0; i < amount; i += 1){
+    arrdiv.push(`<div></div>`)
+  }
+
+
+
+
+  divEl.insertAdjacentHTML('beforeend', arrdiv.join(""))
+
+console.dir(divEl.children);
+
+
+  // divEl.children.style.width = 30;
+  // divEl.children.style.background = "black"
+  
+}
